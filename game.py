ac553b003c02e445bdc45b4cfe9942645360a749
metelyk99 <mukutykartem26@gmail.com>
2024-12-21 11:32:37 +0200

    Create game.py

---
+++ game.py
@@ -0,0 +1,115 @@
+from pygame import *
+
+# клас-батько для спрайтів
+class GameSprite(sprite.Sprite):
+    # конструктор класу
+    def __init__(
+            self,
+            player_image,
+            player_x,
+            player_y,
+            player_speed
+        ):
+        super().__init__()
+        # кожен спрайт повинен зберігати властивість image - зображення
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        # кожен спрайт повинен зберігати властивість rect - 
+        # прямокутник, в який він вписаний
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+# клас-спадкоємець для спрайту-гравця (керується стрілками)
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 60:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 60:
+            self.rect.y += self.speed
+
+# клас-спадкоємець для спрайта-ворога (переміщається сам)
+class Enemy(GameSprite):
+    direction = "left"
+
+    def update(self):
+        if self.rect.x <= 470:
+            self.direction = "right"
+        if self.rect.x >= win_width - 85:
+            self.direction = "left"
+
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+
+# клас для спрайтів-перешкод
+class Wall(sprite.Sprite):
+    def __init__(
+        self,
+        wall_x, 
+        wall_y,
+        wall_width,
+        wall_height,
+        color=(200, 0, 100)
+    ):
+        super().__init__()
+        self.color = color
+        self.width = wall_width
+        self.height = wall_height
+        # картинка стіни - прямокутник потрібних розмірів та кольору
+        self.image = Surface((self.width, self.height))
+        self.image.fill(self.color)
+        # кожен спрайт повинен зберігати властивість rect - прямокутник
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        
+
+# Ігрова сцена:
+win_width = 700
+win_height = 500
+
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load(
+    "background.jpg"), (win_width, win_height))
+
+Player = Player('plyer.png', 5, win_height - 80, 4)
+monster = Enemy('monster.png', win_width - 80, 280, 2)
+final = GameSprite('art.png', win_width - 120, win_width - 80, 0)
+
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.blit(background, (0, 0))
+        Player.update()
+        monster.update()
+
+        Player.reset()
+        monster.reset()
+        final.reset()
+        
+    display.update()
+    clock.tick(FPS)
